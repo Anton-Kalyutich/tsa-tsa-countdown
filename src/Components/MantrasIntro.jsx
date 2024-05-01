d97da7075f560d52ac92fb6771b3b7b186576a54
Anton Kalyutich <anton.kalyutich@gmail.com>
2024-05-01 19:31:27 -0400

    Intro Edited - Now It's About Tsatsas

--- src/Components/MantrasIntro.jsx
+++ src/Components/MantrasIntro.jsx
@@ -10,21 +10,22 @@ const MantrasIntro = () => {
       <p className="intro_title">NRC STUPA OF COMPLETE VICTORY</p>
       {/* if you want to have an image uncomment below */}
       {/* <img src={Logo} alt="Logo" className="intro_image" /> */}
-      {/* <p className="intro_text">
-        Whether you do a formal Guru Yoga session or are doing mantras while
-        walking in the park...if you are dedicating them to{' '}
-        <a href="https://www.diamondway.org/your-project/" target="_new">
-          -- NAME OF THE PROJECT --
-        </a>
-        , make that wish and simply count them and make a record of it. Then
-        input the number of mantras in the form below. Again, you do not have to
-        do a formal sitting session in order to count your Karmapa Chenno
-        mantras. Formal sessions on the 16th Karmapa in your local center are
-        recommended as well. We wish to keep this project ongoing throughout
-        2021 and see how soon we can accomplish <b>-- NUMBER OF MANTRAS--</b>.
-        Primarily we wish to build up Karmapa&apos;s power-field and establish
-        the finest conditions for the this project&apos;s success.
-      </p> */}
+      <p className="intro_text">
+        In September 2019, {<a href='https://lama-ole-nydahl.org/' target="_new">Lama Ole Nydahl</a>} announced the construction of the {<a href="https://www.diamondway.org/nrcstupa/" target="_new">
+          NRC Stupa of Complete Victory
+        </a>}: 
+        “It will be a victory over everything negative, 
+        a victory over things that nobody likes, mental disturbances, 
+        and everything harmful."
+        In November 2022, Lama Ole confirmed the Stupa to be inaugurated in 2024. 
+        He has invited Nedo Kuchung Rinpoche of the Karma Kagyu lineage to oversee its building and inauguration.
+      </p>
+      <br></br>
+      <p className="intro_text">
+        By August 2024, we need to produce approximately <strong>8000</strong> tsatsas,
+        and many friends across the United States (and the world) have already volunteered to help.
+        To coordinate this noble effort we're presenting this tsatsas countdown. Enjoy!
+      </p>
     </div>
   )
 }
